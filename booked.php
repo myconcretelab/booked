e96a612b4022eed6817a4b7ac70f3617a1d4828c
Sebastien Jacqmin <seb@tellthem.be>
2026-07-12 08:13:40 +0200

    Space brass plaque below ornate frame

--- booked.php
+++ booked.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Booked
  * Description: Widget de demande de réservation pour gîtes, relié à l'application contrats.
- * Version: 0.3.71
+ * Version: 0.3.72
  * Author: Sebsoaz
  */
 
@@ -14,7 +14,7 @@ define('BOOKED_PLUGIN_FILE', __FILE__);
 define('BOOKED_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('BOOKED_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('BOOKED_OPTION_KEY', 'booked_settings');
-define('BOOKED_VERSION', '0.3.71');
+define('BOOKED_VERSION', '0.3.72');
 
 require_once BOOKED_PLUGIN_DIR . 'includes/ApiClient.php';
 require_once BOOKED_PLUGIN_DIR . 'includes/Variables.php';
