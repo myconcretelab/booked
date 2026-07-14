35b88499b76dbdd51636bc01546ecc85afc8870d
Sebastien Jacqmin <seb@tellthem.be>
2026-07-14 12:45:16 +0200

    Remove underline from cartel links

--- booked.php
+++ booked.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Booked
  * Description: Widget de demande de réservation pour gîtes, relié à l'application contrats.
- * Version: 0.3.91
+ * Version: 0.3.92
  * Author: Sebsoaz
  */
 
@@ -14,7 +14,7 @@ define('BOOKED_PLUGIN_FILE', __FILE__);
 define('BOOKED_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('BOOKED_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('BOOKED_OPTION_KEY', 'booked_settings');
-define('BOOKED_VERSION', '0.3.91');
+define('BOOKED_VERSION', '0.3.92');
 
 require_once BOOKED_PLUGIN_DIR . 'includes/ApiClient.php';
 require_once BOOKED_PLUGIN_DIR . 'includes/Variables.php';
